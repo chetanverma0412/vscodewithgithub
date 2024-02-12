03d46783d3381c328c9754e85e2c182724bb3a53
Chetan <chetanv.bca2022@ssism.org>
2024-02-12 10:42:04 +0530

    chv

---
+++ date.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class date {
+
+    public static void main(String[] args) {
+        System.out.println("the is frist program");
+        System.out.println("this output of first program ");
+        System.out.println(new Date());
+    }
+}
